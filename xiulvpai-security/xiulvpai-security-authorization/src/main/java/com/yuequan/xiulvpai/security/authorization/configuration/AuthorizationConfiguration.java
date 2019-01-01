f932a647980eaeba8e842225256627d9425b9b54
yuequan <yuequan@localhost.localdomain>
2019-01-01 13:10:34 +0800

    refactor: 修改配置

--- xiulvpai-security/xiulvpai-security-authorization/src/main/java/com/yuequan/xiulvpai/security/authorization/configuration/AuthorizationConfiguration.java
+++ xiulvpai-security/xiulvpai-security-authorization/src/main/java/com/yuequan/xiulvpai/security/authorization/configuration/AuthorizationConfiguration.java
@@ -1,22 +1,15 @@
 package com.yuequan.xiulvpai.security.authorization.configuration;
 
-import com.yuequan.xiulvpai.common.respository.RoleRepository;
 import com.yuequan.xiulvpai.security.authorization.access.AuthorizationAccessDecisionManager;
 import com.yuequan.xiulvpai.security.authorization.access.intercept.UrlAuthorizationFilterInvocationSecurityMetadataSource;
-import com.yuequan.xiulvpai.security.authorization.permission.cache.RolePermission;
+import com.yuequan.xiulvpai.security.authorization.permission.AuthorizationDecisionMaker;
 import com.yuequan.xiulvpai.security.authorization.processor.AuthorizationPostProcessor;
 import com.yuequan.xiulvpai.security.common.configuration.support.registry.AuthorizationRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.security.web.FilterInvocation;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * 授权配置
@@ -32,18 +25,15 @@ public class AuthorizationConfiguration implements AuthorizationRegistry {
     @Autowired
     private UrlAuthorizationFilterInvocationSecurityMetadataSource metadataSource;
 
-    @Autowired
-    private RoleRepository roleRepository;
-
     @Override
     public void configure(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry authorizeRequests) {
         try {
             authorizeRequests
-                    .withObjectPostProcessor(new AuthorizationPostProcessor(accessDecisionManager, metadataSource))
                     .antMatchers("/", "/assets/**")
                     .permitAll()
                     .anyRequest()
-                    .authenticated();
+                    .authenticated()
+                    .withObjectPostProcessor(new AuthorizationPostProcessor(accessDecisionManager, metadataSource));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -59,12 +49,8 @@ public class AuthorizationConfiguration implements AuthorizationRegistry {
         return new UrlAuthorizationFilterInvocationSecurityMetadataSource();
     }
 
-    @Cacheable
-    public Set<RolePermission> cacheRolePermissions(){
-        var rolePermissions = new HashSet<RolePermission>();
-        roleRepository.findAll().forEach(role -> {
-
-        });
-        return rolePermissions;
+    @Bean
+    public AuthorizationDecisionMaker rolePermissionComposite(){
+        return new AuthorizationDecisionMaker();
     }
 }
