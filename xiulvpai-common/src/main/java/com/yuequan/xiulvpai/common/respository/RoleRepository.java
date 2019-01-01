ab5e75e819dcf8bc2e3cc11c9aaa55ca41f19141
yuequan <yuequan@localhost.localdomain>
2019-01-01 12:59:25 +0800

    feat: findAll Add EntityGraph

--- xiulvpai-common/src/main/java/com/yuequan/xiulvpai/common/respository/RoleRepository.java
+++ xiulvpai-common/src/main/java/com/yuequan/xiulvpai/common/respository/RoleRepository.java
@@ -1,6 +1,7 @@
 package com.yuequan.xiulvpai.common.respository;
 
 import com.yuequan.xiulvpai.common.domain.entity.Role;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +13,8 @@ import java.util.List;
  **/
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     List<Role> findByIdIn(Integer[] roleIds);
+
+    @Override
+    @EntityGraph(value = "roles.all", type = EntityGraph.EntityGraphType.FETCH)
+    List<Role> findAll();
 }
