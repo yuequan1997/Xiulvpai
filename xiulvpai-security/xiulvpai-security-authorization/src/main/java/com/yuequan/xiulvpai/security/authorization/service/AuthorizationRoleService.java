1c94c73b62a370870c71b6331adce741c8bcf1e9
yuequan <yuequan@localhost.localdomain>
2019-01-01 13:03:15 +0800

    feat: 定义缓存

---
+++ xiulvpai-security/xiulvpai-security-authorization/src/main/java/com/yuequan/xiulvpai/security/authorization/service/AuthorizationRoleService.java
@@ -0,0 +1,29 @@
+package com.yuequan.xiulvpai.security.authorization.service;
+
+import com.yuequan.xiulvpai.common.domain.entity.Role;
+import com.yuequan.xiulvpai.common.respository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ *
+ * @author yuequan
+ * @since 1.0
+ */
+@Service
+@Transactional(readOnly = true)
+@CacheConfig(cacheNames = "authorization.roles")
+public class AuthorizationRoleService {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Cacheable
+    public List<Role> getRoles(){
+        return roleRepository.findAll();
+    }
+}
